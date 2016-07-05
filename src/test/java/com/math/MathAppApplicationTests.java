5c7dd57ed9c1601729e310ab1614f61e1355815c
ParinGandhi <parin.a.gandhi@gmail.com>
2016-07-05 09:00:46 -0400

    Creating project

---
+++ src/test/java/com/math/MathAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.math;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MathAppApplication.class)
+@WebAppConfiguration
+public class MathAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
